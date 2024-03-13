d78e697c2ba1e8a7cbffa1f6e29e4c79a3e41863
marvin.loth <marvin.loth@stud.th-luebeck.de>
2024-03-13 10:20:25 +0100

    Added team table to database

--- App/Database/schema.sql
+++ App/Database/schema.sql
@@ -5,3 +5,20 @@ CREATE TABLE IF NOT EXISTS user (
     salt VARCHAR,
     last_login DATETIME
 );
+
+CREATE TABLE IF NOT EXISTS team (
+    id INTEGER primary key AUTOINCREMENT,
+    name VARECHAR unique
+);
+
+DELETE FROM team;
+
+INSERT INTO team (name) VALUES ("Ferrari");
+INSERT INTO team (name) VALUES ("Mercedes");
+INSERT INTO team (name) VALUES ("Red Bull");
+INSERT INTO team (name) VALUES ("McLaren");
+INSERT INTO team (name) VALUES ("Aston Martin");
+INSERT INTO team (name) VALUES ("Alpine");
+INSERT INTO team (name) VALUES ("Racing Bulls");
+INSERT INTO team (name) VALUES ("Sauber");
+INSERT INTO team (name) VALUES ("Haas");
